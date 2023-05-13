01204c52b80af7ba2ed9b123557a7d5ca3ca0f00
jellymando <cicikdi@naver.com>
2023-05-13 10:39:08 +0900

    [header, footer] 수정

--- packages/footer/src/Footer.tsx
+++ packages/footer/src/Footer.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
 
 export function Footer() {
-    return (
-        <footer>
-            Footer
-        </footer>
-    );
+  return (
+    <>
+      <footer>Footer</footer>
+      <p>copyright</p>
+    </>
+  );
 }
