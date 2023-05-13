d39e6d3d0686a201db5337e2fb721459998c2dfc
jellymando <cicikdi@naver.com>
2023-05-13 11:51:56 +0900

    [remixapp] 수정

--- packages/remixapp/app/routes/index.tsx
+++ packages/remixapp/app/routes/index.tsx
@@ -1,14 +1,16 @@
-import {Header} from 'header';
-import {Footer} from 'footer';
+import { Header } from "header";
+import { Footer } from "footer";
 
 export default function Index() {
-    return (
-        <>
-            <Header/>
-            <div>
-                Content!
-            </div>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div>
+        나~는~ 행복합니다~
+        <br />
+        나~는~ 행복합니다↗
+      </div>
+      <Footer />
+    </>
+  );
 }
